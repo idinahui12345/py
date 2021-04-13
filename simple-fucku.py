b9e76da35e96206ea101ba5587f26d421f7fcf9d
idinahui12345 <82471358+idinahui12345@users.noreply.github.com>
2021-04-13 22:35:57 +0300

    Create simple-fucku.py
    df

---
+++ simple-fucku.py
@@ -0,0 +1,12 @@
+import socket
+
+HOST, PORT = '', 80
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(100)
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            request_data = conn.recv(1024*8).decode()
+            http_response = b"HTTP/1.1 200 OK\r\n\r\n<html>Hello Friend!</html>"
+            conn.sendall(http_response)
