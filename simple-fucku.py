406805cda264901697f9f57a269905f19a87749b
idinahui12345 <82471358+idinahui12345@users.noreply.github.com>
2021-04-15 00:54:40 +0300

    Update simple-fucku.py

--- simple-fucku.py
+++ simple-fucku.py
@@ -1,12 +1,5 @@
 import socket
 
-HOST, PORT = '', 2080
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen(100)
-    while True:
-        conn, addr = s.accept()
-        with conn:
-            request_data = conn.recv(1024*8).decode()
-            http_response = b"HTTP/1.1 200 OK\r\n\r\n<html>Hello Friend!</html>"
-            conn.sendall(http_response)
+        s.connect(("87.68.221.248",80))
+        s.sendall(b"HELLO FRIENDS!")
